19cd737472cd5b5921f0f16b7647598e8ac40dd5
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-01-13 16:59:27 +0100

    ajouter pagr login avec css

---
+++ src/view/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="../../public/css/login.css">
+</head>
+<body>
+    <canvas id="c"></canvas>
+    <div class="wrapper">
+        <form action="" method="POST">
+            <h2>Login</h2>
+            <div class="input-field">
+                <input type="email" name="email" id="email" required>
+                <label for="email">Enter your email</label>
+            </div>
+            <div class="input-field">
+                <input type="password" name="mode_de_passe" id="mode_de_passe" required>
+                <label for="mode_de_passe">Enter your password</label>
+            </div>
+            <div class="forget">
+                <label for="remember">
+                    <input type="checkbox" id="remember">
+                    <p>Remember me</p>
+                </label>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button type="submit">Log In</button>
+            <button><a href="http://127.0.0.1:5500/index.html">Sign Up</a></button>
+            <div class="register">
+                <p>Don't have an account? <a href="http://127.0.0.1:5500/login/home.html">Register</a></p>
+            </div>
+        </form>
+    </div>
+    <script src="../../public/js/login.js"></script>
+</body>
+</html>
