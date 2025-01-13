bf90d0b6b7bf3796767afed778fc1d974a231024
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-01-13 11:24:54 +0100

    Ajouter un ficher User.php contenant clas User.

---
+++ connection/init.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE croise2;
+
+USE croise2;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+
+-- CREATE TABLE Admin (
+--     id INT PRIMARY KEY,
+--     FOREIGN KEY (id) REFERENCES User(id) ON DELETE CASCADE
+-- );
+
+
+-- CREATE TABLE Teacher (
+--     id INT PRIMARY KEY,
+--     FOREIGN KEY (id) REFERENCES User(id) ON DELETE CASCADE
+-- );
+
+
+-- CREATE TABLE Student (
+--     id INT PRIMARY KEY,
+--     FOREIGN KEY (id) REFERENCES User(id) ON DELETE CASCADE
+-- );
+
+
+CREATE TABLE Course (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    teacher_id INT NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES Teacher(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Enrollment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    enrollment_date DATE NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Notification (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    message TEXT NOT NULL,
+    status ENUM('unread', 'read') NOT NULL,
+    date DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE CourseTags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    course_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO roles (id, name) VALUES (1, "ADMIN");
+INSERT INTO roles (id, name) VALUES (2, "Enseignant");
+INSERT INTO roles (id, name) VALUES (3, "Étudiant");
