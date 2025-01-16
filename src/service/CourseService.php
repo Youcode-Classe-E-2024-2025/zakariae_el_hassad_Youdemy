ee96697d6245e8155906da0e74d1cfdbf83bfbf7
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-01-16 02:42:09 +0100

    ajouter courseService au dossier service

---
+++ src/service/CourseService.php
@@ -0,0 +1,38 @@
+<?php
+
+class CourseService{
+    private CourseDao $courseDao;
+    private CourseTagDao $courseTagDao;
+    private CategoryService $categoryService;
+
+    public function __construct()
+    {
+        $this->courseDao = new CourseDao();
+        $this->courseTagDao = new CoursetagDao();
+        $this->categoryService = new CategoryService();
+    }
+
+    public function save(array $data){
+
+        $category = $this->categoryService->getCategoryById((int)$data["isCategory"]);
+
+        $enseignant = new User();
+        $user = $_SESSION["user"];
+        $enseignant->setId($user->getId());
+        
+       
+        $course = new Course(
+            name : $data["name"],
+            description : $data["description"],
+            enseignant : $enseignant,
+            categoryId : $category,
+        );
+
+        $courseId = $this->courseDao->create($course);
+        $this->courseTagDao->save($courseId, $data["tags"]);
+    }
+
+    public function getAll(){
+        return $this->courseDao->getAll();
+    }
+}
